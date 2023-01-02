eeb50766fa19c04b61b1aa4e2c573eb9427113cd
Stefan Huber <stefan.huber3@baywa.de>
2023-01-02 16:04:07 +0100

    Add cleaned data file

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,12 +1,12 @@
-# Script to train machine learning model.
-
+"""
+    Train the machine learning model
+"""
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
 
-# Add the necessary imports for the starter code.
+data = pd.read_csv('./data/trimmed_census.csv')
 
-# Add code to load in the data.
-
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -19,6 +19,7 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
